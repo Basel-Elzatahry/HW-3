6c0294b76d3f6ab7339b4c1c19e31097c14a95de
Basel <baelzatahry@davidson.edu>
2021-12-04 19:12:09 -0500

    v

--- Bot1.java
+++ Bot1.java
@@ -12,8 +12,44 @@ public class BotOne implements RoShamBot {
       *
       * @return the next action to play.
     */
+    int[][] GLOBAL_TABLE = new int[25][5];
+    HashMap<String, int> states = new HashMap<String, int>();
+    HashMap<Action, String> actions = new HashMap<Action, String>();
+    private void setMap(){
+        String[] moves = {"paper", "rock", "scissors", "lizard", "spock"};
+        int cnt = 0;
+        for (int i=0; i < moves.size(); i++){
+            for (int k = 0; j < moves.size(); j++){
+                String currentState = moves[i]+moves[j];
+                states.put(currentState, cnt);
+                cnt++;
+            }
+        }
+        actions.put(ROCK, "rock");
+        actions.put(PAPER, "paper");
+        actions.put(SCISSORS, "scissors");
+        actions.put(LIZARD, "lizard");
+        actions.put(SPOCK, "spock");
+    }
     public Action getNextMove(Action lastOpponentMove) {
+        //setMap();
+        addToMatrix(lastOpponentMove);
         return lastOpponentMove;
+    }
+    
+    public int[][] getMatrix () {
+        return GLOBAL_TABLE;
+    }
+    
+    public void forgetMatrix() {
+        int[][] empty_table = new int[][];
+        GLOBAL_TABLE = empty_table;
+        
+    }
+    
+    public void addToMatrix(Action lastOpponentMove) {
+        
+        
     }
     
 }
